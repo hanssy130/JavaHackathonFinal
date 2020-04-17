28a810fbb9e98ff25432ca9aee3c5ab507159ec1
RHG <mailboxbrian.d@gmail.com>
2020-04-17 13:16:09 -0700

    Fixed background colour.
    Added button to move scene.
     - function not implemented
    
    Added label to the top as a title.

--- src/FinalProject/MainMenu.java
+++ src/FinalProject/MainMenu.java
@@ -2,11 +2,13 @@ package FinalProject;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class MainMenu extends Application {
@@ -17,28 +19,58 @@ public class MainMenu extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
-
-
-        BorderPane layoutGrid = new BorderPane();
+        BorderPane layoutMainMenu = new BorderPane();
+        layoutMainMenu.setBackground(new Background(
+                new BackgroundFill(Color.rgb(173, 216, 230),
+                        CornerRadii.EMPTY,
+                        Insets.EMPTY
+                        )));
         //Spacing from window border to actual layout is 10px every side.
-        layoutGrid.setPadding(new Insets(10, 10, 10, 10));
+        layoutMainMenu.setPadding(new Insets(10));
 
 
         // Button to go to scene with buttons to time lines
         Button buttonTimelines = new Button("Go to timelines");
 
-        HBox menuBottom = new HBox();
-        menuBottom.getChildren().addAll(buttonTimelines);
+        HBox menuBottomMainMenu = new HBox();
+        menuBottomMainMenu.getChildren().addAll(buttonTimelines);
 
-        layoutGrid.setBottom(menuBottom);
+        layoutMainMenu.setBottom(menuBottomMainMenu);
+
+        Label title = new Label("What is Covid-19?");
+        /*
+        Set font and font size.
+         */
+        title.setFont(new Font("Arial", 30));
+
+        /*
+        This is the HBox to hold the title
+         */
+        HBox menuTop = new HBox();
+        menuTop.getChildren().addAll(title);
+        menuTop.setAlignment(Pos.CENTER);
+
+        layoutMainMenu.setTop(menuTop);
 
 
-        Scene mainScene = new Scene(layoutGrid, 800, 600, Color.LIGHTBLUE);
+        Scene sceneMain = new Scene(layoutMainMenu, 800, 600);
+
+        BorderPane layoutButtons = new BorderPane();
+        layoutButtons.setPadding(new Insets(10));
+
+        /*
+        The different buttons that will appear on the second scene
+         */
+
+
+        
 
 
         primaryStage.setTitle("Main Menu");
-        primaryStage.setScene(mainScene);
+        primaryStage.setScene(sceneMain);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
+
+
 }
