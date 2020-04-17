8d988c2532f887ce652d877048104337793614d3
Justin Majam <pmajam@my.bcit.ca>
2020-04-17 11:04:04 -0700

    working now yay

--- src/test/Lmao.java
+++ src/test/Lmao.java
@@ -1,7 +1,12 @@
-package src.test;
+package test;
 
 public class Lmao {
     Lmao() {
+
         System.out.println("REEEEEEEEEEE");
+    }
+
+    public static void main(String[] args) {
+        Lmao test = new Lmao();
     }
 }
