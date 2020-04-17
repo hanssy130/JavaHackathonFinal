306f10e052da9dc1f539fd141a48fc3814eb56e0
RHG <mailboxbrian.d@gmail.com>
2020-04-17 16:09:24 -0700

    First commit.

---
+++ src/FinalProject/Covid19.java
@@ -0,0 +1,14 @@
+package FinalProject;
+
+import javafx.stage.Stage;
+
+/**
+ * @author Brian Duong
+ * @version 2020
+ */
+public class Covid19 extends Stage {
+
+    public void Info() {
+        
+    }
+}
