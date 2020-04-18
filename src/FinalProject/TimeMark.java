f0d3c23c61396a781ff4a79a856b060673cea874
Justin Majam <pmajam@my.bcit.ca>
2020-04-17 20:31:18 -0700

    changed text in timeMark and Timelines

--- src/FinalProject/TimeMark.java
+++ src/FinalProject/TimeMark.java
@@ -17,6 +17,22 @@ public class TimeMark extends Group {
             + "-fx-max-height: 42px;"
             + "-fx-background-color:#1a1aff ;";
 
+    public TimeMark(String markName, int howFarDownCircle, String description) {
+        final int circleYTranslation = 278;
+        final int radius = 20;
+        Circle timelineMark = new Circle(howFarDownCircle, circleYTranslation, radius);
+        timelineMark.setFill(Color.web("#1a1aff"));
+        Text text = new Text(markName);
+        moveText(text, howFarDownCircle);
+        Button clicker = new Button();
+        buttonStyler(clicker, howFarDownCircle);
+        clicker.setOnAction(actionEvent -> {
+            Stage newWindow = makePopUp(markName, description);
+            newWindow.show();
+        });
+        getChildren().addAll(text, timelineMark, clicker);
+    }
+
     private void moveText(Text text, int howFarDownCircle) {
         final int textYTranslation = 320;
         final int textOffset = 30;
@@ -34,30 +50,18 @@ public class TimeMark extends Group {
     }
 
     private Stage makePopUp(String markName, String description) {
+        final int popUpWidth = 600;
+        final int popUpHeight = 480;
+        final int popUpX = 200;
+        final int popUpY = 100;
         TaiwanEvent hey = new TaiwanEvent(markName, description);
         StackPane secondaryLayout = new StackPane(hey);
-        Scene secondScene = new Scene(secondaryLayout, 600, 480);
+        Scene secondScene = new Scene(secondaryLayout, popUpWidth, popUpHeight);
         Stage newWindow = new Stage();
         newWindow.setTitle("Event");
         newWindow.setScene(secondScene);
-        newWindow.setX(200);
-        newWindow.setY(100);
+        newWindow.setX(popUpX);
+        newWindow.setY(popUpY);
         return newWindow;
-    }
-
-    public TimeMark(String markName, int howFarDownCircle, String description) {
-        final int circleYTranslation = 278;
-        final int radius = 20;
-        Circle timelineMark = new Circle(howFarDownCircle, circleYTranslation, radius);
-        timelineMark.setFill(Color.web("#1a1aff"));
-        Text text = new Text(markName);
-        moveText(text, howFarDownCircle);
-        Button clicker = new Button();
-        buttonStyler(clicker, howFarDownCircle);
-        clicker.setOnAction(actionEvent -> {
-            Stage newWindow = makePopUp(markName, description);
-            newWindow.show();
-        });
-        getChildren().addAll(text, timelineMark, clicker);
     }
 }
