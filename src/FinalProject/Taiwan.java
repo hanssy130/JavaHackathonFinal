4b75a4e89fece8e8b76582f6b0a61fb7f471232c
RHG <mailboxbrian.d@gmail.com>
2020-04-17 21:53:20 -0700

    Adjusted simulation to add a border

--- src/FinalProject/Taiwan.java
+++ src/FinalProject/Taiwan.java
@@ -2,6 +2,8 @@ package FinalProject;
 
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.util.Scanner;
@@ -21,12 +23,16 @@ public class Taiwan extends BouncingBalls {
     public void start() {
 
         Pane canvas = new Pane();
+        Line border = new Line(0, 600, 600, 600);
+        border.setStrokeWidth(3);
+        canvas.getChildren().addAll(border);
         Scene scene = new Scene(canvas, MAX_X, MAX_Y + Y_OFFSET);
 
         final int numberOfBalls = 20;
         generateUninfectedBalls(canvas, numberOfBalls, TAIWAN_MOVE_SPEED, TAIWAN_MOVE_SPEED, TAIWAN_INFECTION_DISTANCE);
         generateInfectedBall(canvas, TAIWAN_MOVE_SPEED, TAIWAN_MOVE_SPEED, TAIWAN_INFECTION_DISTANCE);
 
+        this.initModality(Modality.APPLICATION_MODAL);
         this.setTitle("Taiwan Covid-19 Infection Simulation");
         this.setScene(scene);
         this.show();
