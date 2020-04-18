4dc3b246f2a25e5aa0180d9954e4454be217d53a
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2020-04-17 17:58:33 -0700

    latest commit.

---
+++ src/FinalProject/TaiwanEvent.java
@@ -0,0 +1,23 @@
+package FinalProject;
+
+import javafx.scene.Group;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class TaiwanEvent extends Group {
+
+    private String date;
+    private String description;
+
+    public TaiwanEvent(String date, String description) {
+        this.date = date;
+        this.description = description;
+
+        Text eventDate = new Text(date);
+        Text eventDescription = new Text(description);
+        VBox stackPane = new VBox(eventDate, eventDescription);
+
+        getChildren().addAll(stackPane);
+    }
+}
