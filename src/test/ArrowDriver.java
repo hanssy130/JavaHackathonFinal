fe8722ff632c90f0bd33929c7b18fafaa7cb23ba
Justin Majam <pmajam@my.bcit.ca>
2020-04-17 17:07:31 -0700

    made a timeline class

--- src/test/ArrowDriver.java
+++ src/test/ArrowDriver.java
@@ -1,6 +1,7 @@
 package test;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,8 +12,16 @@ public class ArrowDriver extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Arrow tester = new Arrow();
-        Scene scene = new Scene(tester, 800,  600);
+//        Arrow tester = new Arrow();
+//        TimeMark testMark = new TimeMark("Dec 31",50);
+//        TimeMark testMark2 = new TimeMark("Jan 20",200);
+//        TimeMark testMark3 = new TimeMark("Jan 25",350);
+//        TimeMark testMark4 = new TimeMark("Jan 29",500);
+//        TimeMark testMark5 = new TimeMark("Feb 7",650);
+//        Group display = new Group(tester, testMark, testMark2, testMark3, testMark4, testMark5);
+        String[] stuff = {"yes", "fam"};
+        Timeline test = new Timeline(stuff);
+        Scene scene = new Scene(test.getTimeline(), 800,  600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
---
+++ src/test/Timeline.java
@@ -0,0 +1,39 @@
+package test;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Timeline {
+    private Arrow arrow = new Arrow();
+    private String[] dates;
+    private Group timeline;
+
+    public Timeline(String[] dates) {
+        Group display = new Group();
+        final int dateLimit = 5;
+        if (dates.length > dateLimit) {
+            throw new IllegalArgumentException("Maximum dates per timeline!");
+        }
+        this.dates = dates;
+        display.getChildren().add(arrow);
+        int distance = 50;
+        final int distanceBetweenBalls = 150;
+        for (String date : dates) {
+            TimeMark testMark = new TimeMark(date, distance);
+            display.getChildren().add(testMark);
+            distance += distanceBetweenBalls;
+        }
+        timeline = display;
+    }
+
+    public Group getTimeline() {
+        return timeline;
+    }
+
+    public static void main(String[] args) {
+        String[] stuff = {"yes", "fam"};
+        Timeline test = new Timeline(stuff);
+    }
+}
